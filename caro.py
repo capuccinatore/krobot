a100e9a0d7a386773cff456a460a236ff89ed7c8
Clément Elvira <clementh.elvira@gmail.com>
2015-10-29 21:47:06 +0100

    creating fantastic caro cclass

---
+++ caro.py
@@ -0,0 +1,21 @@
+class Caro(object):
+	"""
+	main caro class
+	"""
+
+	def __init__(self, medium):
+		super(Caro, self).__init__()
+		self.medium = medium
+
+	def run(self):
+		"""  
+		clac clac clac
+		"""
+		pass
+
+	def shutdown(self):
+		""" 
+		allow Caro to sleep
+		"""
+		pass
+		
