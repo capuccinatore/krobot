9bc53c9a96518450ff0bbaaaacf8356a08a0003e
Clément Elvira <clementh.elvira@gmail.com>
2015-10-29 23:05:48 +0100

    adding RU with url

--- krobot/caro.py
+++ krobot/caro.py
@@ -1,3 +1,5 @@
+from data import RU
+
 class Caro(object):
 	"""
 	main caro class
@@ -7,10 +9,17 @@ class Caro(object):
 		super(Caro, self).__init__()
 		self.medium = medium
 
+		ru_barois 	= RU("Barrois"  , "http://www.crous-lille.fr/admin-site/restauration_menu_print_w.php?ru=26&midi=1&soir=1&nb_w=2")
+		ru_parisel  = RU("Pariselle", "http://www.crous-lille.fr/admin-site/restauration_menu_print_w.php?ru=19&midi=1&soir=1&nb_w=2")
+		ru_sully 	= RU("Sully"    , "http://www.crous-lille.fr/admin-site/restauration_menu_print_w.php?ru=25&midi=1&soir=1&nb_w=2")
+
+		self.listRu = [ru_barois, ru_parisel, ru_sully]
+
 	def run(self):
 		"""  
 		clac clac clac
 		"""
+		
 		pass
 
 	def shutdown(self):
@@ -18,4 +27,3 @@ class Caro(object):
 		allow Caro to sleep
 		"""
 		pass
-		
