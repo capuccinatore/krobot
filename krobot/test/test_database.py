b152ccafd6b2aa995ed77d18ddd87fb3c86827af
Clément Elvira <clementh.elvira@gmail.com>
2015-11-12 19:27:11 +0100

    Implementing Database.removeAll and adding its corresponding test

---
+++ krobot/test/test_database.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.getcwd() + '/../')
+
+import pytest
+import database
+
+class TestDatabase:
+
+    def test_one(self):
+
+        db = database.DataBase()
+        db.removeAll()
+
+        assert len(db.constraint) == 0
+        assert len(db.listRu) == 0
