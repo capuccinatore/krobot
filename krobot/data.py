033f04b7d4119b2a5da1e8966a2fb715e3aa878b
Clément Elvira <clementh.elvira@gmail.com>
2015-11-12 22:11:17 +0100

    Implementing menu fetcher + test :)

--- krobot/data.py
+++ krobot/data.py
@@ -1,24 +1,58 @@
-import urllib.request
-import re
+import sys
+import urllib2
+import datetime
+import locale
 
-class RU():
-    """
-    gather daily info on an given restaurant
-    """
+from bs4 import BeautifulSoup
 
-    def __init__(self, name, url):
-        super(RU, self).__init__()
-        self.name = name
-        self.url = url
-        self.boolOpen = 0
-        self.menu = []
+class RU(object):
+	"""
+	gather daily info on an given restaurant
+	"""
 
-    def fetchInformation(self):
-        with urllib.request.urlopen(self.url) as response:
-            self.html = response.read().decode("iso-8859-1")
-            marker = "menu_ru_plat\">"
-            indexStart = self.html.find(marker)
-            indexEnd = self.html.find("<", indexStart)
-            self.menu.append(self.html[indexStart+len(marker):indexEnd])
+	def __init__(self, name, url):
+		super(RU, self).__init__()
+		self.name = name
+		self.url = url
+		self.boolOpen = 0
+		self.menu = []
 
+	def fetchInformation(self):
+		with urllib.request.urlopen(self.url) as response:
+			self.html = response.read().decode("iso-8859-1")
+			marker = "menu_ru_plat\">"
+			indexStart = self.html.find(marker)
+			indexEnd = self.html.find("<", indexStart)
+			self.menu.append(self.html[indexStart+len(marker):indexEnd])
+
+	def fetchInfo(self):
+		"""
+		"""
+		del self.menu[:]
+		response = urllib2.urlopen(self.url)
+		soup = BeautifulSoup(response.read(), "html.parser")
+		days = soup.find_all('table', {"border" : "0", "cellpadding":"0", "cellspacing":"0", "width":"650"})
+
+		current_day = datetime.datetime.now().day;
+		targetting_string = "-1"
+		for day in days:
+			date = day.font.i.b.string
+			list_info = date.split(" ")
+			under_study_day = int(list_info[1])
+			targetting_string = day
+
+		if targetting_string == "-1":
+			raise Exception("Week in " + self.name + "does not match with the current week. :(")
+
+		# Here, targetting_string contains the html code containing the mode (among other things...)
+		a = targetting_string.find_all("font", {"class":"menu_ru_plat"})
+
+		self.menu = map(lambda item: unicode(item.string).encode('utf-8'), day.find_all("font", {"class":"menu_ru_plat"}))
+		print self.menu[1]
+		
+
+if __name__ == '__main__':
+
+    ru = RU("Barrois" ,"http://www.crous-lille.fr/admin-site/restauration_menu_print_w.php?ru=26&midi=1&soir=1&nb_w=2")
+    ru.fetchInfo()
 
--- krobot/test/test_data.py
+++ krobot/test/test_data.py
@@ -4,11 +4,19 @@ import os
 sys.path.insert(0, os.path.dirname(__file__)+"/..")
 import data
 
-def test_RU():
-    testUrlStr = "http://rbardenet.github.io/"
-    ru = data.RU("testRU", testUrlStr)
-    assert ru.url == testUrlStr
-    ru.fetchInformation()
-    assert ru.html[:3] == "<!D" 
+#def test_RU():
+#    testUrlStr = "http://rbardenet.github.io/"
+#    ru = data.RU("testRU", testUrlStr)
+#    assert ru.url == testUrlStr
+#    ru.fetchInformation()
+#    assert ru.html[:3] == "<!D" 
 
+def test_RU_fetchInfo() :
 
+	filetest = "file://" + os.path.dirname(__file__)+"/material/test_data.html"
+	
+	ru = data.RU("Barrois" , filetest)
+	ru.fetchInfo()
+
+	assert len(ru.menu) == 4
+	assert ru.menu[1] == "Darne de saumon"
