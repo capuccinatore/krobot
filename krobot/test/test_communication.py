36f11f1a0cc0bc1a22a2ce1d41a09fc00f7666c2
rbardenet <remi.bardenet@gmail.com>
2015-11-18 22:00:48 +0100

    Added communication class

---
+++ krobot/test/test_communication.py
@@ -0,0 +1,15 @@
+import pytest
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__)+"/..")
+import data
+import datetime
+#import mock
+import time
+import communication
+
+def test_Communication():
+    ruList = []
+    personList = []
+    comm = communication.Communication(personList, ruList)
+    comm.printMenus()
