fad1fe3e2ae167c065177365cb3e327d210489cc
Clément Elvira <clementh.elvira@gmail.com>
2015-11-21 11:21:01 +0100

    Implementing captcha reader (still a few letter missing in dictionary) via brute force

--- krobot/cap.py
+++ krobot/cap.py
@@ -9,25 +9,30 @@ LETTER_SIZE = 8
 LETTER_LEFT = 10
 LETTER_RIGHT = 16
 
-class CreditExtractor(object):
-	"""docstring for CreditExtractor"""
+class CreditFetcher(object):
+	"""docstring for CreditFetcher"""
 
 	def __init__(self):
-		super(CreditExtractor, self).__init__()
+		super(CreditFetcher, self).__init__()
 
 	def process(self):
-		
-		capfile = Image.open("cap/cap.php-1.png")
-		mat_pix = self.openImage(capfile)
-		self.extractSymbol(mat_pix)
 
-		capfile.save("cap/00.png")
+		capReader = captchaReader("Dico")
+		filename = "cap/capt-test.png"
+		cap_str = capReader.read(filename)
+		#capfile.save("cap/00.png")
 
-	def openImage(self, pil_image):
-		
-		return numpy.array(pil_image)
+		return cap_str
 
-	def extractSymbol(self, mat_pix):
+class captchaSymbolExtractor(object):
+	"""docstring for captchaSymbolExtractor"""
+	def __init__(self):
+		super(captchaSymbolExtractor, self).__init__()
+
+	def extractSymbol(self, filename):
+
+		# mat_pix is a numpy array
+		mat_pix = self.openImage(filename)
 
 		list_im = []
 		for i in range(5):
@@ -38,14 +43,60 @@ class CreditExtractor(object):
 			im = Image.fromarray(symb*255)
 			im = im.convert('1')
 
-			im.save("cap/" + str(i) + ".PNG")
-			print numpy.dot(numpy.transpose(symb), symb)
+			#im.save("cap/" + str(i) + ".PNG")
+			#print numpy.dot(numpy.transpose(symb), symb)
 
+			# return a list of numpy array
 		return list_im
 
+	def openImage(self, filename):
 
-ce = CreditExtractor()
-ce.process()
+		pil_image = Image.open(filename)
+		return numpy.array(pil_image)	
 
-im = Image.open("cap/e.png")
-print numpy.array(im)
+
+class captchaReader(object):
+	"""docstring for captchaReader"""
+	def __init__(self, folderDico):
+		super(captchaReader, self).__init__()
+		self.folderDico = folderDico + "/"
+
+	def read(self, filename):
+		
+		# Extract symbol from targetted captcha
+		symb_extractor = captchaSymbolExtractor()
+		listSymb = symb_extractor.extractSymbol(filename)
+
+		cap_string = ""
+		nb_unread = 0
+		for symb in listSymb:
+			succes = False
+			for f in os.listdir(self.folderDico):
+				if f.endswith(".png"):
+					pil_image = Image.open(self.folderDico + f)
+					dic_symb = numpy.array(pil_image)
+					if self.compare(symb, dic_symb):
+						succes = True
+						if f[0].isdigit():
+							cap_string += f[0]
+						else:
+							cap_string += f[3]
+						break
+				
+			if not succes:
+				# If you go there, then the symbol has not been read
+				Image.fromarray(symb).save("error/symb" + str(nb_unread) + ".png")
+				nb_unread += 1
+
+
+		#return the string
+		return cap_string
+
+	def compare(self, symb_np, im_dic):
+		
+		#print symb_np
+		return numpy.array_equal(symb_np, im_dic/255)
+		
+
+#ce = CreditFetcher()
+#assert ce.process() == "7Ks6W"
