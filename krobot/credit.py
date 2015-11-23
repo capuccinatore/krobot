1ab7cec54eb6c0421b5c875ba6f6ba698b206690
Clément Elvira <clementh.elvira@gmail.com>
2015-11-23 23:04:50 +0100

    changing test name + some code

--- krobot/credit.py
+++ krobot/credit.py
@@ -1,13 +1,14 @@
 import os,sys
 from PIL import Image
 import numpy
+import urllib
+import urllib2
+from captcha import CaptchaReader
+from bs4 import BeautifulSoup
+from cookielib import CookieJar
 
 website = "http://reservation.crous-lille.fr:82/index.php"
-LETTER_NB = 5
-LETTER_SPACE = 1
-LETTER_SIZE = 8
-LETTER_LEFT = 10
-LETTER_RIGHT = 16
+my_card = "856328"
 
 class CreditFetcher(object):
 	"""docstring for CreditFetcher"""
@@ -15,88 +16,64 @@ class CreditFetcher(object):
 	def __init__(self):
 		super(CreditFetcher, self).__init__()
 
-	def process(self):
+	def fetch_credit(self, card_str):
 
 		capReader = CaptchaReader("Dico")
-		filename = "cap/capt-test.png"
-		cap_str = capReader.read(filename)
-		#capfile.save("cap/00.png")
-
-		return cap_str
-
-class captchaSymbolExtractor(object):
-	"""docstring for captchaSymbolExtractor"""
-	def __init__(self):
-		super(captchaSymbolExtractor, self).__init__()
-
-	def extractSymbol(self, filename):
-
-		# mat_pix is a numpy array
-		mat_pix = self.openImage(filename)
-
-		list_im = []
-		for i in range(5):
-			left  = LETTER_LEFT + i * (LETTER_SIZE + LETTER_SPACE)
-			right = LETTER_LEFT + (i + 1) * (LETTER_SIZE + LETTER_SPACE) - 1
-			symb = mat_pix[6:19, left:right]
-			list_im.append(symb)
-			im = Image.fromarray(symb*255)
-			im = im.convert('1')
-
-			#im.save("cap/" + str(i) + ".PNG")
-			#print numpy.dot(numpy.transpose(symb), symb)
-
-			# return a list of numpy array
-		return list_im
-
-	def openImage(self, filename):
-
-		pil_image = Image.open(filename)
-		return numpy.array(pil_image)	
-
-
-class CaptchaReader(object):
-	"""docstring for CaptchaReader"""
-	def __init__(self, folderDico):
-		super(CaptchaReader, self).__init__()
-		self.folderDico = folderDico + "/"
-
-	def read(self, filename):
+		cap_str = ""
 		
-		# Extract symbol from targetted captcha
-		symb_extractor = captchaSymbolExtractor()
-		listSymb = symb_extractor.extractSymbol(filename)
+		headers = {
+		'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/537.86.2',
+		}
+		cj = CookieJar()
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+		response = opener.open('http://reservation.crous-lille.fr:82/index.php')
 
-		cap_string = ""
-		nb_unread = 0
-		for symb in listSymb:
-			succes = False
-			for f in os.listdir(self.folderDico):
-				if f.endswith(".png"):
-					pil_image = Image.open(self.folderDico + f)
-					dic_symb = numpy.array(pil_image)
-					if self.compare(symb, dic_symb):
-						succes = True
-						if f[0].isdigit():
-							cap_string += f[0]
-						else:
-							cap_string += f[3]
-						break
-				
-			if not succes:
-				# If you go there, then the symbol has not been read
-				Image.fromarray(symb).save("error/symb" + str(nb_unread) + ".png")
-				nb_unread += 1
+		cookie = response.headers.get('Set-Cookie')
+		phpsesid = self.find_between(cookie, 0, ';')
+		print phpsesid
 
+		req2 = urllib2.Request('http://reservation.crous-lille.fr:82/img.php')
+		req2.add_header('cookie', cookie)
+		f = opener.open(req2)
+		text_req = f.read()
+		print text_req
 
-		#return the string
-		return cap_string
-
-	def compare(self, symb_np, im_dic):
+		req3 = urllib2.Request('http://reservation.crous-lille.fr:82/' + text_req)
+		img = opener.open(req3)
 		
-		#print symb_np
-		return numpy.array_equal(symb_np, im_dic/255)
-		
+		with open('out.png', 'wb') as f:
+			f.write(img.read())
+		pil_image = Image.open('out.png')
+		img = numpy.array(pil_image)
+
+		capReader = CaptchaReader("Dico")
+		captcha_str = capReader.read("out.png")
+		print captcha_str
+
+		values = {'codecap' : captcha_str, 'codecl' : card_str}
+
+		data = urllib.urlencode(values)
+		req_4 = urllib2.Request('http://reservation.crous-lille.fr:82/recup.php', data)
+		rep = opener.open(req_4)#urllib2.urlopen(req_4)
+		info_results = rep.read()
+		print info_results
+
+		soup = BeautifulSoup(info_results, "html.parser")
+		solde = soup.find_all('strong')
+		money_left = self.find_between(str(solde[0]), 8, '<')
+
+		print "You have " + money_left + "on your account, watch out!"
+	
+	def find_between(self, s, index_first, last ):
+		try:
+			start = index_first#s.index( first ) + len( first )
+			end = s.index( last, start )
+			return s[start:end]
+		except ValueError:
+			return ""
+
+
 
 ce = CreditFetcher()
-print ce.process() #== "7Ks6W"
+ce.fetch_credit(my_card)
+
