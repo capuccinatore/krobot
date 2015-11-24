ed75f0452d1c3becfa3cb87598c5bae9d4fd3bb9
Clément Elvira <clementh.elvira@gmail.com>
2015-11-24 23:30:03 +0100

    adding random stuff for travis

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-long_description = read('README.rst')
+long_description = read('README.md')
 
 class PyTest(TestCommand):
     def finalize_options(self):
