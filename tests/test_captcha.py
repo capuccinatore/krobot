50dc1b5d0c2985c4d09c82bfebd6230ed8e7ab06
Clément Elvira <clementh.elvira@gmail.com>
2015-11-24 23:40:17 +0100

    moving test folder

--- tests/test_captcha.py
+++ tests/test_captcha.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.dirname(__file__)+"/..")
+sys.path.insert(0, os.path.dirname(__file__)+"/../krobot/")
 
 from captcha import CaptchaReader
 
